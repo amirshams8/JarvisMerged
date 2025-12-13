146a157e0914812e865061f0408590a5d0608b18
amirshams8 <138038547+amirshams8@users.noreply.github.com>
2025-12-13 20:51:11 +0530

    Update build.gradle.kts

--- modules/engine/build.gradle.kts
+++ modules/engine/build.gradle.kts
@@ -1,16 +1,27 @@
 plugins {
     id("com.android.library")
-    kotlin("android")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.jarvismini.engine"
     compileSdk = 34
-    defaultConfig { minSdk = 26 }
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.json:json:20231013")
-    implementation(kotlin("stdlib"))
 }
