6a43d21dda5535631c25e43d11a41187f530e92b
amirshams8 <138038547+amirshams8@users.noreply.github.com>
2025-12-13 16:29:15 +0530

    Add engine module to project settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "JarvisMerged"
 
 include(":app")
@@ -5,4 +21,4 @@ include(":modules:core")
 include(":modules:automation")
 include(":modules:smart")
 include(":modules:ui")
-include(":modules:engine")   
+include(":modules:engine")
