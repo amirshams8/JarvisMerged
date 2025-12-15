f93f054cd6f287b75a6e8201b916ac8e4c3457e5
amirshams8 <138038547+amirshams8@users.noreply.github.com>
2025-12-15 06:12:43 +0530

    Update MainActivity.kt

--- app/src/main/java/com/jarvismini/MainActivity.kt
+++ app/src/main/java/com/jarvismini/MainActivity.kt
@@ -1,22 +1,17 @@
 package com.jarvismini
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.Intent
-import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
 import com.jarvismini.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var ui: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ui = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(ui.root)
 
-        ui.btnEnableService.setOnClickListener {
-            startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
