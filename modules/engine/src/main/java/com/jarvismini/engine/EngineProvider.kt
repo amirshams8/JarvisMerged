a5ea76411966ae1e0c639ec1b11bc520eec0c7f2
amirshams8 <138038547+amirshams8@users.noreply.github.com>
2025-12-15 07:07:31 +0530

    Update EngineProvider.kt

--- modules/engine/src/main/java/com/jarvismini/engine/EngineProvider.kt
+++ modules/engine/src/main/java/com/jarvismini/engine/EngineProvider.kt
@@ -2,11 +2,7 @@ package com.jarvismini.engine
 
 object EngineProvider {
 
-    val llm: LLMEngine by lazy {
-        StubLLMEngine()
-    }
-
-    val command: CommandEngine by lazy {
-        StubCommandEngine()
+    val engine: LLMEngine by lazy {
+        StubLLMEngine
     }
 }
