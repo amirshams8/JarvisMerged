a1750f3bd8d6ebc0a8a82f54f94b99d38e50ea44
exe5988e-rgb <exe5988e@gmail.com>
2025-12-16 09:23:44 +0530

    Update CoreApp.kt

--- app/src/main/kotlin/com/jarvismini/CoreApp.kt
+++ app/src/main/kotlin/com/jarvismini/CoreApp.kt
@@ -1,14 +1,13 @@
 package com.jarvismini
 
 import android.app.Application
-import com.jarvismini.engine.EngineProvider
 
 class CoreApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        // Initialize LLM engine (stub or real, selected internally)
-        EngineProvider.engine.init(this)
+        // Engine wiring will be added later
+        // Keeping app startup clean for now
     }
 }
