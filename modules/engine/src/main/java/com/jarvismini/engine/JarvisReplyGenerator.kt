dcbb5201154f21b8160a1422efec99d9aa652242
exe5988e-rgb <exe5988e@gmail.com>
2025-12-16 07:13:55 +0530

    Update JarvisReplyGenerator to generate replies
    Refactor reply generation to use new method with incoming message.

--- modules/engine/src/main/java/com/jarvismini/engine/JarvisReplyGenerator.kt
+++ modules/engine/src/main/java/com/jarvismini/engine/JarvisReplyGenerator.kt
@@ -2,15 +2,14 @@ package com.jarvismini.engine
 
 object JarvisReplyGenerator {
 
-    fun generateReply(input: AutoReplyInput): String {
-        // TEMPORARY STUB (until real LLM is wired)
-        return when (input.reason) {
-            ReplyReason.USER_BUSY ->
-                "This is Jarvis, assisting Mr. Aamir. He is currently occupied and will respond when available."
-            ReplyReason.MISSED_MESSAGE ->
-                "Jarvis here, assisting Mr. Aamir. He will review this message shortly."
-            ReplyReason.IMPORTANT_MESSAGE ->
-                "This is Jarvis, assisting Mr. Aamir. He has been notified and will respond as soon as possible."
-        }
+    fun generate(
+        incomingMessage: String,
+        reason: ReplyReason,
+        mode: JarvisMode
+    ): String {
+
+        val baseReply = ReplyToneResolver.resolve(reason, mode)
+
+        return "$baseReply\n\nMessage received: \"$incomingMessage\""
     }
 }
